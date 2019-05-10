8d0dcc2dd38aa74c9a7323e6b43074a4277ea404
taylorreiter <taylorreiter@gmail.com>
2019-05-10 15:39:51 -0700

    add tanglegram

--- KEGGDecoder/KEGG_decoder.py
+++ KEGGDecoder/KEGG_decoder.py
@@ -1212,6 +1212,59 @@ def default_viz(genome_df):
 	fig.set_size_inches(100, 100)
 	fig.savefig("function_heatmap.svg")
 
+def make_tanglegram(genome_df, newick):
+    import matplotlib.pyplot as plt
+    import itertools
+    from Bio import Phylo
+    import tanglegram as tg
+    from scipy.cluster.hierarchy import ward, complete, average, dendrogram, fcluster, linkage
+    
+    # FORMAT KEGGDECODER OUTPUT
+    # generate distance matrix for genome_df from pathway values
+    genome_df = pd.read_csv(genome_df, index_col=0, sep='\t')
+    kegg_d = squareform(pdist(genome_df, metric='euclidean'))
+    kegg_m = pd.DataFrame(kegg_d)
+    kegg_m.columns = genome_df.index.tolist()
+    kegg_m.index = genome_df.index.tolist()
+    kegg_m = kegg_m.reindex(sorted(kegg_m.columns), axis=1) # reorder column names alphabetically
+    kegg_m.sort_index(inplace=True) # reorder row names alphabetically
+    
+    # FORMAT NEWICK FILE
+    # generate distance matrix from newick file
+    tree = Phylo.read(newick, 'newick')
+    
+    tree_d = {}
+    for x, y in itertools.combinations(tree.get_terminals(), 2):
+        v = tree.distance(x, y)
+        tree_d[x.name] = tree_d.get(x.name, {})
+        tree_d[x.name][y.name] = v
+        tree_d[y.name] = tree_d.get(y.name, {})
+        tree_d[y.name][x.name] = v
+        
+    for x in tree.get_terminals():
+        tree_d[x.name][x.name] = 0
+    
+    tree_m = pd.DataFrame(tree_d)
+    tree_m = tree_m.reindex(sorted(tree_m.columns), axis=1) # reorder column names alphabetically
+    tree_m.sort_index(inplace=True) # reorder row names alphabetically
+    
+    # TANGLEGRAM
+    kegg_labels = kegg_m.columns.values.tolist()
+    tree_labels = tree_m.columns.values.tolist()
+
+    kegg_mat = pd.DataFrame(kegg_m,
+                        columns=kegg_labels,
+                        index=kegg_labels)
+
+    tree_mat = pd.DataFrame(tree_m,
+                        columns=tree_labels,
+                        index=tree_labels)
+
+    # Plot and try to minimize cross-over
+    fig = tg.gen_tangle(kegg_mat, tree_mat, optimize_order=1000)
+    fig.set_size_inches(10, 10)
+    fig.savefig("function_newick_tanglegram.svg")
+
 def main():
 	import matplotlib
 	matplotlib.use('Agg')
@@ -1229,6 +1282,7 @@ def main():
 	parser.add_argument('-o', '--output', help="List version of the final heat\
 						map figure")
 	parser.add_argument('-v', '--vizoption', help="Options: static, interactive, tanglegram")
+        parser.add_argument('--newick', help="Required input for tanglegram visualization")
 	args = parser.parse_args()
 	arg_dict = vars(args)
 
@@ -1377,6 +1431,8 @@ def main():
 	if arg_dict['vizoption'] == 'interactive':
 		from .Plotly_viz import plotly_viz
 		plotly_viz(genome)
+        if arg_dict['vizoption'] == 'tangelgram':
+                make_tanglegram(genome,str(arg_dict['newick']))
 
 if __name__ == "__main__":
 	main()
